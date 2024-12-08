b8ebfca173b0ded65036e317a153f56234136b11
demirose41 <demirose41@gmail.com>
2024-12-07 18:41:11 -0800

    completed bonus

--- sql-practice/aggregates.sql
+++ sql-practice/aggregates.sql
@@ -3,3 +3,18 @@ SELECT COUNT(*) AS total_cats FROM cats;
 SELECT MIN(birth_year) AS oldest, name FROM cats;
 
 SELECT MAX(birth_year) AS youngest, name FROM cats;
+
+-- Bonus
+
+SELECT cats.name ,COUNT(*) AS toy_count 
+FROM toys 
+JOIN cats 
+  ON toys.cat_id = cats.id 
+GROUP BY cats.name;
+
+SELECT cats.name ,COUNT(*) AS toy_count 
+FROM toys 
+JOIN cats ON toys.cat_id = cats.id 
+GROUP BY cats.name 
+HAVING toy_count > 1;
+
